ee2b9aefb1213c519d1b6ed0b09afd8bc8d5e8f4
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-03-24 17:14:41 +0200

    Add files via upload

---
+++ ConsoleApp27/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Account
+    {
+        private static int NumberOfAcc;
+        private readonly int accountNumber;
+        private readonly Customer accountOwner;
+        private int maxMinusAllowed;
+        public int AccountNumber { get; }
+        public int Balance { get; private set; }
+        public Customer AccountOwner { get;}
+        public int MaxMinusAllowed { get;}
+
+
+
+    }
+}
---
+++ ConsoleApp27/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Customer
+    {
+        private static int NumberOfCust;
+        private readonly int customerID;
+        private readonly int customerNumber;
+        public string Name { get; private set; }
+        public int PhNumber { get; private set; }
+        public int CustomerID { get; }
+        public int CustomerNumber { get; }
+
+        public Customer(string name, int phNumber, int customerID)
+        {
+            Name = name;
+            PhNumber = phNumber;
+            CustomerID = customerID;
+        }
+
+    }
+}
